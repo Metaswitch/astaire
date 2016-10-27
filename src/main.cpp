1ce98c57bbba040e1be4598a3460589c89448b05
Graeme Robertson <graeme.robertson@metaswitch.com>
2016-10-27 23:48:45 +0000

    Use real syslog

--- src/main.cpp
+++ src/main.cpp
@@ -231,11 +231,6 @@ int main(int argc, char** argv)
   options.pidfile = "";
   options.daemon = false;
 
-  // Initialise ENT logging before making "Started" log
-  PDLogStatic::init(argv[0]);
-
-  CL_ASTAIRE_STARTED.log();
-
   if (init_logging_options(argc, argv, options) != 0)
   {
     return 1;
@@ -247,6 +242,10 @@ int main(int argc, char** argv)
                           options.log_directory,
                           options.log_level,
                           options.log_to_file);
+
+  // We should now have a connection to syslog so we can write the started ENT
+  // log.
+  CL_ASTAIRE_STARTED.log();
 
   std::stringstream options_ss;
   for (int ii = 0; ii < argc; ii++)
