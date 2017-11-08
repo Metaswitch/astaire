01c20412745c18071867e8b9a9e775e1aade1a8d
Mat Meredith <matthew.meredith@metaswitch.com>
2017-11-08 07:56:29 +0000

    Make timeout half the length of that used by Sprout / Homestead to allow for retries

--- src/memcached_backend.cpp
+++ src/memcached_backend.cpp
@@ -58,7 +58,7 @@ MemcachedBackend::MemcachedBackend(MemcachedConfigReader* config_reader,
   // timeout because libmemcached tries to connect to all servers sequentially
   // during start-up, and if any are not up we don't want to wait for any
   // significant length of time.
-  _options = "--CONNECT-TIMEOUT=10 --SUPPORT-CAS --POLL-TIMEOUT=250 --BINARY-PROTOCOL";
+  _options = "--CONNECT-TIMEOUT=10 --SUPPORT-CAS --POLL-TIMEOUT=50 --BINARY-PROTOCOL";
 
   // Create an updater to keep the store configured appropriately.
   _updater = new Updater<void, MemcachedBackend>(this, std::mem_fun(&MemcachedBackend::update_config));
